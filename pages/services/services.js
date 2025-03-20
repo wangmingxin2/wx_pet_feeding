d3f82e9e179076249a5e4c0918a093fcc9dcf7e5
wmx <1556710059@qq.com>
2025-03-20 20:40:27 +0800

    【新增】完成服务页面

--- pages/services/services.js
+++ pages/services/services.js
@@ -1,8 +1,44 @@
+const app = getApp()
+import request from '../../utils/request'
+
 Page({
   data: {
-    serviceList: []
+    services: []
   },
-  onLoad: function () {
-    // 页面加载时获取服务列表
+
+  onLoad() {
+    this.fetchServices()
+  },
+
+  onShow() {
+    this.fetchServices()
+  },
+
+  fetchServices() {
+    request({
+      url: '/service/search?status=ACTIVE',
+      method: 'POST'
+    }).then(res => {
+      if (res.code === 200) {
+        this.setData({
+          services: res.data
+        })
+      } else {
+        throw new Error(res.msg || '获取服务列表失败')
+      }
+    }).catch(err => {
+      console.error('获取服务列表失败：', err)
+      wx.showToast({
+        title: '获取服务列表失败',
+        icon: 'none'
+      })
+    })
+  },
+
+  onTapService(e) {
+    const service = e.currentTarget.dataset.service
+    wx.navigateTo({
+      url: `/pages/order/create?serviceId=${service.serviceId}`
+    })
   }
 }) 
